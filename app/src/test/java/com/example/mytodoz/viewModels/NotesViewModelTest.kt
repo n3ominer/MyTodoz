4ceee3f45aa517143797f1fbd09ca0607c701bfb
Ramzy KERMAD <ramzy.kermad@edenred.com>
2025-11-26 09:12:42 +0100

    Unit test implementation

---
+++ app/src/test/java/com/example/mytodoz/viewModels/NotesViewModelTest.kt
@@ -0,0 +1,16 @@
+package com.example.mytodoz.viewModels
+
+
+// Create FakeRepoSuccess
+
+// Create FakeRepoFailure
+
+
+class NotesViewModelTest {
+
+    // Setup Asyn behavior
+
+    // Write test for NoteVm fetchNotesFromRepo()
+
+  
+}
